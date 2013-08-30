d3afa10e7c56ad8590302b9328441f3fb85dbdf9
Johannes Gehrs <jgehrs@example.com>
2013-08-30 17:47:48 +0200

    increases version to 0.1.2

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup
 
 setup(
     name='WTForms-ParsleyJS',
-    version='0.1.1',
+    version='0.1.2',
     url='https://github.com/johannes-gehrs/wtforms-parsleyjs',
     license='MIT',
     author='Johannes Gehrs',
